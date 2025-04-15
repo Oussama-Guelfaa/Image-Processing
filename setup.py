e0aa77ca06104ff45d3280e97da4b017d694e9c9
Oussama-Guelfaa <guelfaao@gmail.com>
2025-04-15 15:15:30 +0200

    Mise à jour du système d'installation pour résoudre les conflits de dépendances

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from setuptools import setup, find_packages
+import os
+
+# Lire le README.md s'il existe, sinon utiliser une description par défaut
+long_description = "A collection of image processing tools for educational purposes."
+if os.path.exists("README.md"):
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
 
 setup(
-    name="image-processing-tools",
+    name="imgproc-tools",  # Changement de nom pour éviter les conflits
     version="0.1.0",
     author="Oussama GUELFAA",
     author_email="your.email@example.com",
     description="A collection of image processing tools",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Oussama-Guelfaa/Image-Processing",
+    url="https://github.com/yourusername/Image-Processing",
     packages=find_packages(),
     include_package_data=True,
     classifiers=[
@@ -18,9 +25,9 @@ setup(
     ],
     python_requires=">=3.6",
     install_requires=[
-        "numpy",
-        "matplotlib",
-        "scikit-image",
+        "numpy>=1.19.0",
+        "matplotlib>=3.3.0",
+        "scikit-image>=0.17.0",
     ],
     entry_points={
         "console_scripts": [
